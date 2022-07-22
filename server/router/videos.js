44991bac70da0d137ffc3ebd16ef15be6cbc7ebe
bluepebble25 <bluepebble25@gmail.com>
2022-07-22 17:38:50 +0900

    feat: add Landing Page

--- server/router/videos.js
+++ server/router/videos.js
@@ -92,4 +92,13 @@ router.post('/uploadVideo', (req, res) => {
   });
 });
 
+router.get('/', (req, res) => {
+  Video.find()
+    .populate('writer')
+    .exec((err, videos) => {
+      if(err) return res.status(400).send(err);
+      res.status(200).json({ videos });
+    });
+});
+
 module.exports = router;
